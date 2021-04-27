0dd7de7f2c85536cf549666b99b36df684028805
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-04-27 11:26:14 +0200

    Change env config names

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    apiUrl: process.env.API_URL || "http://localhost:5001",
-    quaverBaseUrl: process.env.QUAVER_BASE_URL || "https://quavergame.com",
-    quaverCdnUrl: process.env.QUAVER_CDN_URL || "https://cdn.quavergame.com"
+    apiUrl: process.env.VUE_APP_API_URL || "http://localhost:5001",
+    quaverBaseUrl: process.env.VUE_APP_QUAVER_BASE_URL || "https://quavergame.com",
+    quaverCdnUrl: process.env.VUE_APP_QUAVER_CDN_URL || "https://cdn.quavergame.com"
 };
 
 export default config;
