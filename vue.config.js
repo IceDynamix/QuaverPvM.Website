5b776f0393e3ca92db76de42a436994690ef5964
AiAe <bgdppeu@gmail.com>
2021-05-18 12:56:42 +0300

    Move css to public, implement revision versioning

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
+const revision = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString().trim();
+
 module.exports = {
     chainWebpack(config) {
         config
             .plugin('html')
             .tap(args => {
                 args[0].title = "QuaverPvM";
+                args[0].version = revision;
                 return args;
             });
     }
