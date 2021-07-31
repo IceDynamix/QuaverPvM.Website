9c94408bddc88aebf07b0fd2bc3c0e3dfef52254
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-31 14:16:32 +0200

    Add stats to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,40 +9,80 @@
                 </i>
             </p>
         </div>
+        <transition name="transition" mode="out-in">
+            <div class="stats" v-if="stats != null">
+                <div class="stat">
+                    <h4 class="stat-title">Users (ranked)</h4>
+                    <p class="stat-value">
+                        <Number :value="stats['userCount']" />
+                        (<Number :value="stats['rankedUserCount']" />)
+                    </p>
+                </div>
+                <div class="stat">
+                    <h4 class="stat-title">Maps (ranked)</h4>
+                    <p class="stat-value">
+                        <Number :value="stats['mapCount']" />
+                        (<Number :value="stats['rankedMapCount']" />)
+                    </p>
+                </div>
+                <div class="stat">
+                    <h4 class="stat-title">Matches played</h4>
+                    <p class="stat-value">
+                        <Number :value="stats['matchCount']" />
+                    </p>
+                </div>
+            </div>
+        </transition>
         <div class="features">
-            <div class="features cols">
-                <div class="col">
-                    <h3>Rating System</h3>
-                    <p class="feature-text">
-                        Imagine chess, where two players play against each
-                        other. Each of them has an rating, that goes up or down
-                        depending on whether they win or lose. QuaverPvM uses
-                        that concept and applies it to a VSRG environment. You
-                        have a rating that goes up or down as you win or lose,
-                        and the map also has a rating that goes up or down. The
-                        win condition in QuaverPvM is reaching 95%.
-                    </p>
-                </div>
-                <div class="col">
-                    <h3>Skill Range</h3>
-                    <p class="feature-text">
-                        Get map "recommendations" in your skill range. You'll
-                        always have something challenging to play. The rating
-                        system will make sure it's mostly accurate, and if it's
-                        not, then the outcome will make sure that it becomes
-                        more accurate over time. If you're not feeling the
-                        thrill of playing the ranked mode, then you can always
-                        play the unranked mode without worrying about your
-                        rating.
-                    </p>
-                </div>
+            <div class="col feature">
+                <h3>Rating System</h3>
+                <p class="feature-text">
+                    Imagine chess, where two players play against each other.
+                    Each of them has a rating, that goes up or down depending on
+                    whether they win or lose. QuaverPvM uses that concept and
+                    applies it to a VSRG environment.
+                </p>
+                <p class="feature-text">
+                    You have a rating that goes up or down as you win or lose,
+                    and the map also has a rating that goes up or down. The win
+                    condition in QuaverPvM is reaching 95%.
+                </p>
+            </div>
+            <div class="col feature">
+                <h3>Skill Range</h3>
+                <p class="feature-text">
+                    Get map "recommendations" in your skill range. You'll always
+                    have something challenging to play. The rating system will
+                    make sure it's mostly accurate. If it's not, then the
+                    outcome will make sure that it becomes more accurate over
+                    time.
+                </p>
+                <p class="feature-text">
+                    If you're not feeling the thrill of playing the ranked mode,
+                    then you can always play the unranked mode without worrying
+                    about your rating (coming soon).
+                </p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import Number from "@/components/Elements/ElementNumber.vue";
+
+export default {
+    components: { Number },
+    data() {
+        return {
+            stats: null,
+        };
+    },
+    async created() {
+        const { data } = await axios.get("stats");
+        this.stats = data;
+    },
+};
 </script>
 
 <style scoped>
@@ -61,19 +101,30 @@ export default {};
 .title > * {
     text-align: center;
 }
-.features {
+.stats {
     display: flex;
     align-items: center;
     flex-direction: row;
     flex-wrap: wrap;
     width: 100%;
 }
-.col {
+.stat {
+    flex: 200px;
+    text-align: center;
+}
+.stat-value {
+    margin-top: -10px;
+}
+.features {
+    display: flex;
+    align-items: top;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+}
+.feature {
     flex: 300px;
     margin: 25px;
-}
-.feature-text {
-    text-align: justify;
 }
 .feature-icon {
     height: 35px;
