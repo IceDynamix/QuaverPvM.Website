35f2b9b74ea5f914edad9fe3c7018d24b943743e
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-08-04 17:01:47 +0200

    Add basic match page

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,11 @@ import axios from "axios";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: { user: null },
+    state: {
+        user: null,
+        match: null,
+        matchLoading: false
+    },
     actions: {
         async login({ commit }) {
             try {
@@ -18,10 +22,28 @@ export default new Vuex.Store({
                 Vue.toasted.show(err.response.data.err, { type: "error" });
             }
         },
+        async fetchOngoingMatch({ commit }) {
+            commit("setMatchLoading", true);
+            try {
+                const { data: match } = await axios.get("match/ongoing");
+                commit("setMatch", match);
+            } catch (err) {
+                console.error(err.response.data.err);
+                Vue.toasted.show(err.response.data.err, { type: "error" });
+            } finally {
+                commit("setMatchLoading", false);
+            }
+        },
     },
     mutations: {
         setUser(state, { user }) {
             state.user = user;
+        },
+        setMatch(state, match) {
+            state.match = match;
+        },
+        setMatchLoading(state, loading) {
+            state.loading = loading;
         },
     },
     plugins: [Vuex.createLogger()]
---
+++ src/views/Play.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="play">
+        <div class="flavor tutorial" v-if="!user || user.matchesPlayed < 5">
+            Welcome to QuaverPvM! This is your first time playing, so here's
+            what you need to know to get going. The instructions will disappear
+            after a match, but will be available in the FAQ at any time.
+            <ol>
+                <li>
+                    Once you click on MATCH, you will be matched against a map.
+                </li>
+                <li>
+                    After getting matched, you have 10 minutes to submit an S
+                    rank play on the map. Mirror and higher rates are allowed.
+                </li>
+                <li>
+                    When your score is submitted to Quaver, you need to click
+                    the SUBMIT button. Your recent plays will be scanned for a
+                    valid play. If you're unable to submit an S rank play, then
+                    you're free to resign with the RESIGN button.
+                </li>
+                <li>
+                    Your rating is going to be hidden until you've finished your
+                    10 placement matches.
+                </li>
+                <li>
+                    Feel free to check out the FAQ or ask questions in the
+                    Discord.
+                </li>
+            </ol>
+        </div>
+        <div
+            class="flavor helper"
+            v-if="user.rd > 100 && user.matchesPlayed > 0"
+        >
+            Your Rating Deviation (currentlt {{ user.rd.toFixed(0) }} RD) is
+            above 100, which means you're <b>unranked</b>. Once your RD is below
+            100, you will receive a letter rank and appear on the leaderboards
+            (coming soon!).
+            <!-- TODO Edit when leaderboards are out -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        user() {
+            return this.$store.state.user;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.flavor {
+    margin: 25px;
+}
+</style>
