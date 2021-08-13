d119edb7c9580e8588a205fff5320361c92e7872
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-08-13 17:24:54 +0200

    Show tutorial

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,42 +1,49 @@
 <template>
     <div class="play">
-        <div class="flavor" v-if="showTutorial">
-            Welcome to QuaverPvM! This is your first time playing, so here's
-            what you need to know to get going. The instructions will disappear
-            after a match, but will be available in the FAQ at any time.
-            <ol>
-                <li>
-                    Once you click on MATCH, you will be matched against a map.
-                </li>
-                <li>
-                    After getting matched, you have 10 minutes to submit an S
-                    rank play on the map. Mirror and higher rates are allowed.
-                </li>
-                <li>
-                    When your score is submitted to Quaver, you need to click
-                    the SUBMIT button. Your recent plays will be scanned for a
-                    valid play. If you're unable to submit an S rank play, then
-                    you're free to resign with the RESIGN button.
-                </li>
-                <li>
-                    Your rating is going to be hidden until you've finished your
-                    10 placement matches.
-                </li>
-                <li>
-                    Feel free to check out the FAQ or ask questions in the
-                    Discord.
-                </li>
-            </ol>
+        <div v-if="user">
+            <div class="flavor" v-if="showTutorial">
+                Welcome to QuaverPvM! This is your first time playing, so here's
+                what you need to know to get going. The instructions will
+                disappear after a match, but will be available in the FAQ at any
+                time.
+                <ol>
+                    <li>
+                        Once you click on MATCH, you will be matched against a
+                        map.
+                    </li>
+                    <li>
+                        After getting matched, you have 10 minutes to submit an
+                        S rank play on the map. Mirror and higher rates are
+                        allowed.
+                    </li>
+                    <li>
+                        When your score is submitted to Quaver, you need to
+                        click the SUBMIT button. Your recent plays will be
+                        scanned for a valid play. If you're unable to submit an
+                        S rank play, then you're free to resign with the RESIGN
+                        button.
+                    </li>
+                    <li>
+                        Your rating is going to be hidden until you've finished
+                        your 10 placement matches.
+                    </li>
+                    <li>
+                        Feel free to check out the FAQ or ask questions in the
+                        Discord.
+                    </li>
+                </ol>
+            </div>
+            <div class="flavor" v-if="showUnranked">
+                Your Rating Deviation (currently {{ user.rd.toFixed(0) }} RD) is
+                above 100, which means you're <b>unranked</b>. Once your RD is
+                below 100, you will receive a letter rank and appear on the
+                leaderboards (coming soon!).
+                <!-- TODO Edit when leaderboards are out -->
+            </div>
+            <hr v-if="showTutorial || showUnranked" />
+            <Match />
         </div>
-        <div class="flavor" v-if="showUnranked">
-            Your Rating Deviation (currently {{ user.rd.toFixed(0) }} RD) is
-            above 100, which means you're <b>unranked</b>. Once your RD is below
-            100, you will receive a letter rank and appear on the leaderboards
-            (coming soon!).
-            <!-- TODO Edit when leaderboards are out -->
-        </div>
-        <hr v-if="showTutorial || showUnranked" />
-        <Match />
+        <div v-else>Please login to play!</div>
     </div>
 </template>
 
@@ -48,6 +55,9 @@ export default {
         Match,
     },
     computed: {
+        user() {
+            return this.$store.state.user;
+        },
         showTutorial() {
             return this.user && this.user.matchesPlayed === 0;
         },
